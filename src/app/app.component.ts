531395f00ffd713e3bc86ad019f69dc5b58e5449
Hugues Marandeau <h.marandeau@gmail.com>
2023-01-02 14:58:58 +0100

    first commit, v0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
+import { Observable } from 'rxjs';
+
+interface FileToDisplay {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,82 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+  public columnDefs: ColDef[] = [];
+  public defaultColDef: ColDef = {
+    sortable: true,
+    filter: true,
+    resizable: true,
+  };
+  public rowData$?: any[];
+  fileList: FileToDisplay[] = [];
+  selectedFile: string = "";
+  selectedDate = "";
+  selectedItem?: {
+    data: Object,
+    linkHref: string,
+    linkDisplayedText: string
+  };
+//   selectedItemLink?: string;
+
+  // For accessing the Grid's API
+  @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
+
+
+  changeFileSelected(newSelectedFile: any) {
+    console.log("ok");
+    fetch(`assets/${this.selectedDate}/${newSelectedFile}.json`).then(res => res.json()).then(
+      jsonData => {
+        this.columnDefs = [];
+        jsonData["columns"].map((element: any) => this.columnDefs.push({field: element}));
+        this.rowData$ = jsonData["items"];
+      }
+    );
+  }
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    fetch(`assets/latest_generated.json`).then(res => res.json()).then(
+      jsonData => {
+        this.selectedDate = jsonData["folder"];
+        fetch(`assets/${this.selectedDate}/list_of_files.json`).then(res => res.json()).then(
+          fileList => {
+            fileList.sort();
+            fileList.forEach((fileName: string) => {
+              let viewValue = fileName.replace(/_/g, " ");
+              this.fileList.push({value: fileName, viewValue: viewValue});
+            });
+//             this.selectedFile = this.fileList[0].value;
+//             this.changeFileSelected(this.selectedFile);
+          }
+        );
+      }
+    );
+  }
+
+  // Example of consuming Grid Event
+  onCellClicked( e: CellClickedEvent): void {
+//     console.log('cellClicked', e.data);
+//         console.log(this.selectedItem);
+
+    this.selectedItem = {
+      data: e.data,
+      linkHref: `https://universalis.app/market/${e.data['_id']}`,
+      linkDisplayedText: e.data['Item Name']
+    }
+//     this.selectedItemLink = `https://universalis.app/market/${e.data['_id']}`
+//     console.log(this.selectedItemLink);
+//     console.log(this.selectedItem);
+  }
+
+//   (click)="onSelect(hero)"
+
+  // Example using Grid's API
+  clearSelection(): void {
+    this.agGrid.api.deselectAll();
+  }
+
+  openDialog() {
+    console.log("open dialog");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
+import { AgGridModule } from 'ag-grid-angular';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ declarations: [
+   AppComponent
+ ],
+ imports: [
+   BrowserModule,
+   HttpClientModule,
+   AgGridModule,
+   FormsModule,
+   ReactiveFormsModule,
+   BrowserAnimationsModule,
+   MatSlideToggleModule,
+   MatButtonModule,
+   MatSelectModule,
+   MatInputModule,
+   MatFormFieldModule
+ ],
+ providers: [],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
