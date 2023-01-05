2849817473573e0d36a48394fa6da9d4f9dcc69c
Hugues Marandeau <h.marandeau@gmail.com>
2023-01-05 15:55:49 +0100

    Move history into a different folder that was updated to include server, timeframe and date selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,22 +23,36 @@ export class AppComponent {
   };
   public rowData$?: any[];
   fileList: FileToDisplay[] = [];
-  selectedFile: string = "";
-  selectedDate = "";
   selectedItem?: {
     data: Object,
     linkHref: string,
     linkDisplayedText: string
   };
+
+  fullTreeInfo?: any;
+
+  selectedShoppingListInfo?: {
+    server: string,
+    timeframe: string,
+    dateGenerated: string,
+    shoppingList: string,
+  }
+
+  selectedServer: string = "";
+  selectedTimeFrame: string = "";
+  selectedDate: string = "";
+  selectedShoppingList: string = "";
+
+  FFXIVServers?: any = [];
+  shoppingListTimeframes?: any = [];
+  availableDates?: any = [];
 //   selectedItemLink?: string;
 
   // For accessing the Grid's API
   @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
 
-
-  changeFileSelected(newSelectedFile: any) {
-    console.log("ok");
-    fetch(`assets/${this.selectedDate}/${newSelectedFile}.json`).then(res => res.json()).then(
+  updateTableData() {
+    fetch(`assets/history/${this.selectedServer}/${this.selectedTimeFrame}/${this.selectedDate}/${this.selectedShoppingList}`).then(res => res.json()).then(
       jsonData => {
         this.columnDefs = [];
         jsonData["columns"].map((element: any) => this.columnDefs.push({field: element}));
@@ -47,40 +61,127 @@ export class AppComponent {
     );
   }
 
+
+  changeFileSelected(newSelectedFile: any) {
+    this.selectedShoppingList = newSelectedFile;
+    console.log("ok");
+    console.log(newSelectedFile);
+    console.log(this.selectedDate);
+    this.updateTableData();
+//     fetch(`assets/history/${this.selectedServer}/${this.selectedTimeFrame}/${this.selectedDate}/${this.selectedShoppingList}`).then(res => res.json()).then(
+//       jsonData => {
+//         this.columnDefs = [];
+//         jsonData["columns"].map((element: any) => this.columnDefs.push({field: element}));
+//         this.rowData$ = jsonData["items"];
+//       }
+//     );
+  }
+
+  changeServerSelected(newSelectedFFXIVServer: any){
+    console.log(newSelectedFFXIVServer);
+    this.selectedServer = newSelectedFFXIVServer;
+
+    this.shoppingListTimeframes = Object.keys(this.fullTreeInfo["history"][this.selectedServer]);
+    this.selectedTimeFrame = this.shoppingListTimeframes[0];
+
+    this.availableDates = Object.keys(this.fullTreeInfo["history"][this.selectedServer][this.selectedTimeFrame]);
+    this.availableDates.sort();
+    this.selectedDate = this.availableDates[0];
+
+    this.fileList = [];
+    this.fullTreeInfo["history"][this.selectedServer][this.selectedTimeFrame][this.selectedDate].sort().forEach(
+      (fileName: any) => {
+        let viewValue = fileName.replace(/_/g, " ").replace(/.json/g, "");
+        this.fileList.push({value: fileName, viewValue: viewValue});
+      }
+    );
+
+    this.selectedShoppingList = this.fileList[0].value;
+    this.updateTableData();
+  }
+
+  changeSelectedTimeFrame(newSelectedTimeFrame: any){
+    console.log(newSelectedTimeFrame);
+    this.selectedTimeFrame = newSelectedTimeFrame;
+
+    this.availableDates = Object.keys(this.fullTreeInfo["history"][this.selectedServer][this.selectedTimeFrame]);
+    this.availableDates.sort();
+    this.selectedDate = this.availableDates[0];
+
+    this.fileList = [];
+    this.fullTreeInfo["history"][this.selectedServer][this.selectedTimeFrame][this.selectedDate].sort().forEach(
+      (fileName: any) => {
+        let viewValue = fileName.replace(/_/g, " ").replace(/.json/g, "");
+        this.fileList.push({value: fileName, viewValue: viewValue});
+      }
+    );
+
+    this.selectedShoppingList = this.fileList[0].value;
+    this.updateTableData();
+  }
+
+  changeSelectedDate(newSelectedDate: any){
+    console.log(newSelectedDate);
+    this.selectedDate = newSelectedDate;
+
+    this.fileList = [];
+    this.fullTreeInfo["history"][this.selectedServer][this.selectedTimeFrame][this.selectedDate].sort().forEach(
+      (fileName: any) => {
+        let viewValue = fileName.replace(/_/g, " ").replace(/.json/g, "");
+        this.fileList.push({value: fileName, viewValue: viewValue});
+      }
+    );
+
+    this.selectedShoppingList = this.fileList[0].value;
+    this.updateTableData();
+  }
+
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    fetch(`assets/latest_generated.json`).then(res => res.json()).then(
+    fetch(`assets/history_tree.json`).then(res => res.json()).then(
       jsonData => {
-        this.selectedDate = jsonData["folder"];
-        fetch(`assets/${this.selectedDate}/list_of_files.json`).then(res => res.json()).then(
-          fileList => {
-            fileList.sort();
-            fileList.forEach((fileName: string) => {
-              let viewValue = fileName.replace(/_/g, " ");
-              this.fileList.push({value: fileName, viewValue: viewValue});
-            });
-            this.selectedFile = this.fileList[0].value;
-            this.changeFileSelected(this.selectedFile);
+        console.log(jsonData);
+        this.fullTreeInfo = jsonData;
+
+        this.FFXIVServers = Object.keys(jsonData["history"]);
+        this.FFXIVServers.sort();
+        this.selectedServer = this.FFXIVServers[1];
+
+        this.shoppingListTimeframes = Object.keys(jsonData["history"][this.selectedServer]);
+        this.selectedTimeFrame = this.shoppingListTimeframes[0];
+
+        this.availableDates = Object.keys(jsonData["history"][this.selectedServer][this.selectedTimeFrame]);
+        console.log(this.availableDates);
+        this.availableDates.sort(function(a: string, b: string) {
+          // Folders have the date written in a format YYYY-MM-DD
+          // Potentially could add -HH-MM-SS in a future improvement, but this should be backward compatible as all new
+          // folders will be created with this format too.
+          a = a.split('-').join('');
+          b = b.split('-').join('');
+          return a > b ? -1 : a < b ? 1 : 0;
+        });
+        this.selectedDate = this.availableDates[0];
+
+        jsonData["history"][this.selectedServer][this.selectedTimeFrame][this.selectedDate].sort().forEach(
+          (fileName: any) => {
+            let viewValue = fileName.replace(/_/g, " ").replace(/.json/g, "");
+            this.fileList.push({value: fileName, viewValue: viewValue});
           }
         );
+
+        this.selectedShoppingList = this.fileList[0].value;
+        this.updateTableData();
       }
     );
   }
 
-  // Example of consuming Grid Event
   onCellClicked( e: CellClickedEvent): void {
-//     console.log('cellClicked', e.data);
-//         console.log(this.selectedItem);
-
     this.selectedItem = {
       data: e.data,
       linkHref: `https://universalis.app/market/${e.data['_id']}`,
       linkDisplayedText: e.data['Item Name']
     }
-//     this.selectedItemLink = `https://universalis.app/market/${e.data['_id']}`
-//     console.log(this.selectedItemLink);
-//     console.log(this.selectedItem);
   }
 
 //   (click)="onSelect(hero)"
